f4c28b05494d60a835fe3b45da25fc6136294ad6
calwen <hooljun@163.com>
2017-05-06 22:58:43 +0800

    master change

--- User.java
+++ User.java
@@ -1,10 +1,10 @@
 public class User{
 	
 	public static void main(String[] args){
+		System.out.println("=====master--start==!!!");
+		System.out.println("=====master--start==!!!");
 		System.out.println("=====brh--start==!!!");
-		System.out.println("=====呵呵呵呵呵--start==!!!");
-		System.out.println("=====brh--start==!!!");
-		System.out.println("=====brh--start==!!!");
+		System.out.println("=====master--start==!!!");
 		System.out.println("======brh-end====!!!");
 	}
 }
