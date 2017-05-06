02971060bc80ec3f243a4848f79d49558760f777
calwen <hooljun@163.com>
2017-05-06 22:29:02 +0800

    change user master

--- User.java
+++ User.java
@@ -1,13 +1,7 @@
 public class User{
 	
 	public static void main(String[] args){
-		System.out.println("hello 哈哈哈!!!");
-		System.out.println("hello 哈哈哈!!!");
-		System.out.println("hello 哈哈哈!!!");
-
-		System.out.println("hello 哈哈哈!!!");
-		System.out.println("hello 哈哈哈!!!");
-		System.out.println("hello 哈哈哈!!!");
-		System.out.println("hello git!!!");
+		System.out.println("=====master--start==!!!");
+		System.out.println("======master-end====!!!");
 	}
 }
