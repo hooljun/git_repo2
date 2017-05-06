0348f820a6db23be4c41bbcec0ad6577f0e81c49
calwen <hooljun@163.com>
2017-05-06 22:58:00 +0800

    brh change

--- User.java
+++ User.java
@@ -2,7 +2,7 @@ public class User{
 	
 	public static void main(String[] args){
 		System.out.println("=====brh--start==!!!");
-		System.out.println("=====呵呵呵呵呵--start==!!!");
+		System.out.println("=====BTH--start--start==!!!");
 		System.out.println("=====brh--start==!!!");
 		System.out.println("=====brh--start==!!!");
 		System.out.println("======brh-end====!!!");
