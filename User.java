6c7ee8dda99b73488dcfa2460caaa2c3d6503822
calwen <hooljun@163.com>
2017-05-06 22:30:17 +0800

    change user brh

--- User.java
+++ User.java
@@ -1,7 +1,10 @@
 public class User{
 	
 	public static void main(String[] args){
-		System.out.println("=====master--start==!!!");
-		System.out.println("======master-end====!!!");
+		System.out.println("=====brh--start==!!!");
+		System.out.println("=====brh--start==!!!");
+		System.out.println("=====brh--start==!!!");
+		System.out.println("=====brh--start==!!!");
+		System.out.println("======brh-end====!!!");
 	}
 }
