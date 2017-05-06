a17ab2cde2268a47a0581529bb57b0f5b93d4f90
calwen <hooljun@163.com>
2017-05-06 20:51:46 +0800

    Add new line for brh

--- Demo.java
+++ Demo.java
@@ -2,6 +2,14 @@ public class Hello{
 	
 	public static void main(String[] args){
 		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
 		System.out.println("hello git!!!");
 	}
 }
