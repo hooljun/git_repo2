a10d137baaef8693e0bcb998f5866b68969bfd09
calwen <hooljun@163.com>
2017-05-07 11:04:01 +0800

    patch apply

--- Hello.java
+++ Hello.java
@@ -3,6 +3,9 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("hello git!!!");
 		
-		System.out.println("hello git!!!");
+		System.out.println("patch git!!!");
+		System.out.println("patch git!!!");
+		System.out.println("patch git!!!");
+		System.out.println("patch git!!!");
 	}
 }
