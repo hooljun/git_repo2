0effdd6f8217f19c3fb6a6bde194498665c6b6cd
calwen <hooljun@163.com>
2017-05-07 11:47:07 +0800

    dev1 add Dev1.java

---
+++ Dev1.java
@@ -0,0 +1,15 @@
+public class Hello{
+	
+	public static void main(String[] args){
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello 哈哈哈!!!");
+		System.out.println("hello git!!!");
+	}
+}
